33923c7add77d6f1787489a474d5ee0075526d1b
Robert <rwatt@four51.com>
2014-02-12 17:40:36 -0600

    Added and styled Aside navigation for top level categories on mobile

--- app/js/directives/tree.js
+++ app/js/directives/tree.js
@@ -18,12 +18,12 @@ four51.app.directive('node', function($compile) {
 			node: '='
 		},
 		template: '<li><a ng-href="#/catalog/{{node.InteropID}}" ng-bind-html="node.Name"></a></li>',
-		link: function(scope, element) {
-			if (angular.isArray(scope.node.SubCategories)) {
-				element.append("<categorytree tree='node.SubCategories' />");
-				$compile(element.contents())(scope);
-			}
-		}
+//		link: function(scope, element) {
+//			if (angular.isArray(scope.node.SubCategories)) {
+//				element.append("<categorytree tree='node.SubCategories' />");
+//				$compile(element.contents())(scope);
+//			}
+//		}
 	};
 	return obj;
 });
